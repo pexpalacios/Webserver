d70d7e424645466f9f29e450195438f5ae7cbe33
penpalac <penpalac@student.42madrid.com>
2026-03-23 14:24:56 +0100

    Made CGIs more protected in case of error i script.js. Added a new possible error to change_outfit.py

--- www/amiwuevo/cgi-bin/change_outfit.py
+++ www/amiwuevo/cgi-bin/change_outfit.py
@@ -15,21 +15,28 @@ outfits = [
 script_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(script_dir, "../../../database/outfit.txt")
 
-#Read current background
+
+#Read current outfit
 try:
     with open(db_path, "r") as f:
         current = f.read().strip()
 except FileNotFoundError:
     current = outfits[0]
 
-#Find next background and write it in file
+#Find next outfit and write it in file
 try:
     idx = outfits.index(current)
     next_fit = outfits[(idx + 1) % len(outfits)]
 except ValueError:
     next_fit = outfits[0]
 
-with open(db_path, "w") as f:
-    f.write(next_fit)
+try:
+	with open(db_path, "w") as f:
+    	f.write(next_fit)
+except PermissionError:
+	next_fit = outfits[0]
+
 
 print(next_fit)
+
+##check file permissions
